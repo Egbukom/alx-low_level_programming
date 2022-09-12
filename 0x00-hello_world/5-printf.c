2b11eb4dc64c70d95a3356cc5a7d030c9ca2ad3d
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-12 16:50:10 +0000

    Printf, came back for betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/* ALX first task, but i've added int to the main function */
+/**
+ * main - executes first
+ * Description: print a sentence
+ * Return: value of main
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
