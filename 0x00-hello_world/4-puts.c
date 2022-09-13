cca97a779b5c8607964bd49efd0b811eb9dc06d5
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-13 07:15:55 +0000

    Committing puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - still don't quite get it
+ * Description: printing text using puts
+ * Puts: the printer
+ * Return: don't get it as a matter of fact
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
