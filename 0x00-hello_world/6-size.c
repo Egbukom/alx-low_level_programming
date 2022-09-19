0edac305a7a2b42de897335f5e82acf5d50bc3f5
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-19 14:47:37 +0000

    Finally working again

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - executes program
+* description: prints sizes
+* Return: zero
+*/
+int main(void)
+{
+	int char;
+
+	printf("Size of a char: %d (bytes)\n", sizeof(char));
+	return (0);
+}
