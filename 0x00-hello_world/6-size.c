35ae7749e048977911c8dc293d0307ff0b894f2e
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-21 15:01:45 +0000

    committing size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - executes program
-* description: prints sizes
+* main - entry point
+* prints sizes of some data types
 * Return: zero
 */
 int main(void)
 {
-	int char;
-
-	printf("Size of a char: %d (bytes)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
---
+++ 0x00-hello_world/sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: zero
+*/
+int main(void)
+{
+	printf("The size of an int is: %lu-byte", sizeof(int));
+	return (0);
+}
