531c93f63f58b5df9dfe1229bd018ecbc0a3c185
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-23 20:58:07 +0000

    Committing islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+/**
+* _islower - checks whether a character belongs,
+* to the lowercase character class.
+* Return: zero
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
