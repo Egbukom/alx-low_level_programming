4c9cb7525a1872fd6d3cc057a407c0f424165de3
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-23 22:28:30 +0000

    Committing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+* _abs - finds the absolute value of a number
+* @int: integer variable
+* Return: zero
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
