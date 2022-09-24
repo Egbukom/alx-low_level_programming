f99f5ede27babcb1143bfc99b9ed2e23c52095d6
Egbukom <ugbekilenwamegbukom@gmail.com>
2022-09-24 00:03:08 +0000

    Commit adds two vars

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - sum up two integers
+* @x: integer variable
+* @y: integer variable
+* Return: zero
+*/
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
